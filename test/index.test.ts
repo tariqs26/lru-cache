f75989acdf9c691ab552e1546b50ac74d1111dfc
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-22 05:05:43 -0500

    test: improve code coverage by increasing size of max capacity, add tests for lfu cache. add tests for clear method

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,9 @@
-import { MRUCache } from "./../src/caches/mru-cache"
 import type { CacheOptions } from "../src"
-import { SimpleCache, LRUCache, FIFOCache } from "../src"
+import { SimpleCache, LRUCache, MRUCache, FIFOCache, LFUCache } from "../src"
 import { CacheSync } from "../src/abstract-classes"
 
 const cacheOptions: CacheOptions = {
-  maxCapacity: 2,
+  maxCapacity: 3,
 }
 
 describe("Cache module", () => {
@@ -16,6 +15,7 @@ describe("Cache module", () => {
       new FIFOCache(cacheOptions),
       new LRUCache(cacheOptions),
       new MRUCache(cacheOptions),
+      new LFUCache(cacheOptions),
     ]
   })
 
@@ -24,25 +24,30 @@ describe("Cache module", () => {
       cache.set("users", [{ id: 1, name: "John doe" }])
       expect(cache.get("users")).toStrictEqual([{ id: 1, name: "John doe" }])
       expect(cache.capacity).toBe(1)
-      
+
       cache.set("users", [])
       expect(cache.get("users")).toStrictEqual([])
-      
+
       cache.set("enabled", true)
       expect(cache.get("enabled")).toBe(true)
       expect(cache.capacity).toBe(2)
-      
+
       cache.set("gpu", "RTX 4090")
       expect(cache.get("gpu")).toBe("RTX 4090")
+      expect(cache.capacity).toBe(3)
+
+      cache.set("list", null)
+      expect(cache.get("list")).toBeNull()
 
       if (cache instanceof MRUCache) {
-        expect(cache.get("enabled")).toBeUndefined()
-        expect(cache.get("users")).toStrictEqual([])
-        expect(cache.capacity).toBe(2)
+        expect(cache.get("gpu")).toBeUndefined()
+        expect(cache.capacity).toBe(3)
       } else if (cache instanceof LRUCache) {
-        expect(cache.get("enabled")).toBe(true)
         expect(cache.get("users")).toBeUndefined()
-        expect(cache.capacity).toBe(2)
+        expect(cache.capacity).toBe(3)
+      } else if (cache instanceof LFUCache) {
+        expect(cache.capacity).toBe(3)
+        expect(cache.get("enabled")).toBeUndefined()
       }
     }
   })
@@ -62,6 +67,17 @@ describe("Cache module", () => {
       expect(cache.get("count")).toBeUndefined()
       expect(cache.capacity).toBe(0)
       cache.remove("key_not_in_cache")
+      expect(cache.capacity).toBe(0)
+    }
+  })
+
+  it("clear", () => {
+    for (const cache of caches) {
+      cache.set("count", 0)
+      cache.set("name", "Test Name")
+      cache.clear()
+      expect(cache.get("count")).toBeUndefined()
+      expect(cache.get("name")).toBeUndefined()
       expect(cache.capacity).toBe(0)
     }
   })
