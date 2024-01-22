ec4b790b3110bffb9ae580c290096e4c5e9e7e5a
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-22 02:15:23 -0500

    test: extend to work with fifo and mru cache implementations

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,22 @@
-import { SimpleCache, LRUCache } from "../src"
+import { MRUCache } from "./../src/caches/mru-cache"
+import type { CacheOptions } from "../src"
+import { SimpleCache, LRUCache, FIFOCache } from "../src"
 import { CacheSync } from "../src/abstract-classes"
+
+const cacheOptions: CacheOptions = {
+  maxCapacity: 2,
+}
 
 describe("Cache module", () => {
   let caches: CacheSync[]
 
   beforeEach(() => {
-    caches = [new SimpleCache(), new LRUCache()]
+    caches = [
+      new SimpleCache(),
+      new FIFOCache(cacheOptions),
+      new LRUCache(cacheOptions),
+      new MRUCache(cacheOptions),
+    ]
   })
 
   it("set", () => {
@@ -13,11 +24,26 @@ describe("Cache module", () => {
       cache.set("users", [{ id: 1, name: "John doe" }])
       expect(cache.get("users")).toStrictEqual([{ id: 1, name: "John doe" }])
       expect(cache.capacity).toBe(1)
+      
       cache.set("users", [])
       expect(cache.get("users")).toStrictEqual([])
+      
       cache.set("enabled", true)
       expect(cache.get("enabled")).toBe(true)
       expect(cache.capacity).toBe(2)
+      
+      cache.set("gpu", "RTX 4090")
+      expect(cache.get("gpu")).toBe("RTX 4090")
+
+      if (cache instanceof MRUCache) {
+        expect(cache.get("enabled")).toBeUndefined()
+        expect(cache.get("users")).toStrictEqual([])
+        expect(cache.capacity).toBe(2)
+      } else if (cache instanceof LRUCache) {
+        expect(cache.get("enabled")).toBe(true)
+        expect(cache.get("users")).toBeUndefined()
+        expect(cache.capacity).toBe(2)
+      }
     }
   })
 
