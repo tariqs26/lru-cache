3621ca17a38e5bec3fd60be43eead2c5f9532690
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-22 02:10:47 -0500

    refactor: add protected evict method to CacheSync, remove cacheAsync

--- src/abstract-classes.ts
+++ src/abstract-classes.ts
@@ -1,4 +1,4 @@
-import { CacheOptions, CachePromise, CacheValue } from "./types"
+import { CacheOptions, CacheValue } from "./types"
 
 const MIN_MAX_CAPACITY = 1
 const MAX_MAX_CAPACITY = 1000000
@@ -33,6 +33,11 @@ export abstract class CacheSync extends BaseCache {
   abstract set(key: PropertyKey, value: CacheValue): void
 
   /**
+   * Internal strategy for determining which entry to remove when the capacity limit is reached
+   */
+  protected abstract evict(): void
+
+  /**
    * Retrieve entry from cache
    * @param {PropertyKey} key - Key associated with the entry
    * @returns {CacheValue} - Value stored in the entry
@@ -44,28 +49,4 @@ export abstract class CacheSync extends BaseCache {
    * @param {PropertyKey} key - Key for the entry
    */
   abstract remove(key: PropertyKey): void
-}
-
-export abstract class CacheAsync extends BaseCache {
-  /**
-   * Create or update entry in cache
-   * @param {PropertyKey} key - Key for the entry
-   * @param {CacheValue} value - Value to be stored
-   * @returns {Promise}
-   */
-  abstract set(key: PropertyKey, value: CacheValue): Promise<void>
-
-  /**
-   * Retrieve entry from cache
-   * @param {PropertyKey} key - Key associated with the entry
-   * @returns {Promise<CacheValue | undefined>} Stored value if found otherwise undefined
-   */
-  abstract get(key: PropertyKey): CachePromise
-
-  /**
-   * Remove entry from cache
-   * @param {PropertyKey} key - Key for the entry
-   * @returns {Promise}
-   */
-  abstract remove(key: PropertyKey): Promise<void>
 }
