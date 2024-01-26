3b5b9659d5ae051ec6e975afd3a568e854e6f2cc
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-26 01:11:18 -0500

    test: add dll tests

---
+++ test/dll.test.ts
@@ -0,0 +1,84 @@
+import { DLL } from "../src/data-structures/dll"
+import { DLLNode } from "../src/data-structures/dll-node"
+
+describe("DLL module", () => {
+  let list: DLL<number>
+
+  beforeEach(() => {
+    list = new DLL()
+  })
+
+  test("insert", () => {
+    list.insert(10)
+    expect(list.size).toBe(1)
+    expect(list.head?.data).toBe(10)
+    expect(list.tail?.data).toBe(10)
+
+    list.insert(20)
+    expect(list.size).toBe(2)
+    expect(list.head?.data).toBe(20)
+    expect(list.tail?.data).toBe(10)
+  })
+
+  test("insertAtTail", () => {
+    list.insertAtTail(10)
+    expect(list.size).toBe(1)
+    expect(list.head?.data).toBe(10)
+    expect(list.tail?.data).toBe(10)
+
+    list.insertAtTail(20)
+    expect(list.size).toBe(2)
+    expect(list.head?.data).toBe(10)
+    expect(list.tail?.data).toBe(20)
+  })
+
+  test("remove", () => {
+    // Removing node from empty list
+    expect(list.remove(new DLLNode(20))).toBeUndefined()
+
+    const tail = list.insert(10)
+    const toRemove = list.insert(20)
+    const head = list.insert(30)
+
+    // Node removed is between head and tail
+    const middleNodeData = list.remove(toRemove)
+
+    expect(middleNodeData).toBe(20)
+    expect(list.head).toStrictEqual(head)
+    expect(list.tail).toStrictEqual(tail)
+    expect(list.size).toBe(2)
+
+    // Node removed is the tail
+    const tailData = list.remove(tail)
+
+    expect(tailData).toBe(10)
+    expect(list.head).toStrictEqual(head)
+    expect(list.tail).toStrictEqual(head)
+    expect(list.size).toBe(1)
+
+    // Node removed is the head
+    const headData = list.remove(head)
+
+    expect(headData).toEqual(30)
+    expect(list.head).toBeNull()
+    expect(list.size).toBe(0)
+  })
+
+  test("removeHead", () => {
+    const head = list.insert(10)
+    list.insert(20)
+    const removedData = list.removeHead()
+    expect(list.head).toStrictEqual(head)
+    expect(list.size).toBe(1)
+    expect(removedData).toBe(20)
+  })
+
+  test("removeTail", () => {
+    list.insert(10)
+    const tail = list.insert(20)
+    const removedData = list.removeTail()
+    expect(list.tail).toStrictEqual(tail)
+    expect(list.size).toBe(1)
+    expect(removedData).toBe(10)
+  })
+})
