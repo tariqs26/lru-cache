d8a011ab94d8b4d34913c676528b0c0db24982b4
Saad Tariq <saadtariq.dev@outlook.com>
2024-07-13 18:59:06 -0400

    refactor: remove format option from tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  format: ["cjs", "esm"], // transpile to esm or cjs
   entry: ["./src/index.ts"],
   dts: true, // declarations
   shims: true,
